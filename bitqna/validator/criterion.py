d037517e2d51c11db21c32aa6315b5f92cd791ab
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-01-07 17:55:01 +0000

    more detail being provided to the miner

--- bitqna/validator/criterion.py
+++ bitqna/validator/criterion.py
@@ -40,45 +40,55 @@ class Criterion():
         self.eval_fx = eval_fx
         self.eval_args = eval_args
 
-    def evaluate(self, validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, str]:
-        reward, feedback = self.eval_fx(validator, response, *self.eval_args)
+    def evaluate(self, task, validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, float, str]:
+        reward, max_reward, feedback = self.eval_fx(task, validator, response, *self.eval_args)
         feedback = f"[bold blue]{self.name}[/bold blue]\n" + feedback
-        return reward, feedback
+        return reward, max_reward, feedback
 
     def __repr__(self):
         return pformat(vars(self), indent=4, width=1)
 
 # CRITERION: successful call to miner
-def does_not_error(validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, str]:
+def does_not_error(task, validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, float, str]:
     max_reward = 0.5
     a_status_code = response.axon.status_code
     d_status_code = response.dendrite.status_code
     reward = -0.5
-    feedback = bad_message("You failed to respond correctly to the request.")
-    if a_status_code == "200" and d_status_code == "200":
+    if a_status_code == 200 and d_status_code == 200:
         reward = 0.5
         feedback = good_message("You successfully responded to the request.")
-    return reward, feedback + received_reward_template(reward, max_reward)
+    else:
+        feedback = bad_message("You failed to respond correctly to the request.")
+        if d_status_code == 408:
+            feedback += "You timed out and will fail the remainder of the criteria."
+        feedback += f" Status Code: {a_status_code}/{d_status_code}"
+        
+    return reward, max_reward, feedback + received_reward_template.format(reward, max_reward)
 
 # CRITERION: reward speedy response
-def does_not_take_a_long_time(validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, str]:
+def does_not_take_a_long_time(task, validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, float, str]:
     max_reward = 0.5
     process_time = response.dendrite.process_time
+    if not process_time:
+        feedback = f"You likely ran into an error processing this task and failed to respond appropriately."
+        reward = 0
+        return reward, max_reward, bad_message(feedback) + received_reward_template.format(reward,max_reward)
+        
     feedback = f"You responded to the request in {process_time}."
     reward = 0.0
     if process_time <= 2.0: 
         reward = 0.50
-        return reward, good_message(feedback) + received_reward_template.format(reward,max_reward)
+        return reward, max_reward, good_message(feedback) + received_reward_template.format(reward,max_reward)
     if process_time <= 5.0:
         reward = 0.25
-        return reward, good_message(feedback) + received_reward_template.format(reward,max_reward)
+        return reward, max_reward, good_message(feedback) + received_reward_template.format(reward,max_reward)
     if process_time <= 10.0:
         reward = 0.10
-        return reward, bad_message(feedback) + received_reward_template.format(reward,max_reward)
-    return reward, bad_message(feedback) + received_reward_template.format(reward,max_reward)
+        return reward, max_reward, bad_message(feedback) + received_reward_template.format(reward,max_reward)
+    return reward, max_reward, bad_message(feedback) + received_reward_template.format(reward,max_reward)
 
 # CRITERION: reward proper citation dict format
-def correct_citation_format(validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, str]:
+def correct_citation_format(task, validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, float, str]:
     max_reward = 1.0
     try:
         citations = response.response['citations']
@@ -87,19 +97,19 @@ def correct_citation_format(validator: BaseValidatorNeuron, response: bt.Synapse
     except KeyError:
         reward = -0.5
         feedback = bad_message("You failed to provide the correct citation format, see details in the protocol.")
-        return reward, feedback+received_reward_template.format(reward, max_reward)
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
     if len(sources) > 0 and len(contexts) == len(sources):
         reward = max_reward
         feedback = good_message("You successfully provided the correct citation format.")
-        return reward, feedback+received_reward_template.format(reward, max_reward)
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
     reward = 0.0
     feedback = bad_message("You failed to provide citations or there is a mismatch count between sources and contexts.")
-    return reward, feedback+received_reward_template.format(reward, max_reward)
+    return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
 # CRITERION: reward proper number (or range) of citations returned
-def contains_number_citations(validator: BaseValidatorNeuron, response: bt.Synapse, min_citations=1, max_citations=None) -> [float, str]:
+def contains_number_citations(task, validator: BaseValidatorNeuron, response: bt.Synapse, min_citations=1, max_citations=None) -> [float, float, str]:
     max_reward = 1.0
     try:
         citations = response.response['citations']
@@ -107,19 +117,19 @@ def contains_number_citations(validator: BaseValidatorNeuron, response: bt.Synap
         # no citations provided and no placeholder available
         reward = -0.5
         feedback = bad_message(f"Failed to provide citations or the correct number of citations in the range of {min_citations} to {max_citations}.")
-        return reward, feedback+received_reward_template.format(reward, max_reward)
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
     if (not min_citations or len(citations) >= min_citations) and (not max_citations or len(citations) <= max_citations):
         reward = max_reward
         feedback = good_message(f"You provided the correct number of citations in the range of {min_citations} to {max_citations}.")
-        return reward, feedback+received_reward_template.format(reward, max_reward)
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
     # placeholder for citations, but none
     reward = -0.25
     feedback = bad_message(f"Failed to provide the correct number of citations in the range of {min_citations} to {max_citations}.")
-    return reward, feedback+received_reward_template.format(reward, max_reward)
+    return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
 # CRITERION: reward inclusion of correct source data in citations
-def contains_correct_number_of_citation_sources(validator: BaseValidatorNeuron, response: bt.Synapse, selected_datas: List[dict]=[], selected_urls: List[str]=[]) -> [float, str]:
+def contains_correct_citation_source(task, validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, float, str]:
     max_reward = 1.5
     try:
         citations = response.response['citations']
@@ -129,7 +139,35 @@ def contains_correct_number_of_citation_sources(validator: BaseValidatorNeuron, 
         # but not being evaluated by this criterion (see contains_number_citationS)
         reward = -0.5
         feedback = bad_message(f"Failed to provide the correct response formatting, see protocol definition.")
-        return reward, feedback+received_reward_template.format(reward, max_reward)
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
+
+    score = 0.0
+    for source in sources:
+        if source == task.citation_sources_should_contain:
+            score=max_reward
+            break
+
+    reward = score
+    if score > 0.0:
+        feedback = good_message(f"You correctly identified the source.")
+    else:
+        feedback = bad_message(f"You failed to correctly identify the correct citation source.") + f"Expected: {task.citation_sources_should_contain}, Received: {sources}"
+    return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
+
+
+
+# CRITERION: reward inclusion of correct source data in citations
+def contains_correct_number_of_citation_sources(task, validator: BaseValidatorNeuron, response: bt.Synapse, selected_datas: List[dict]=[], selected_urls: List[str]=[]) -> [float, float, str]:
+    max_reward = 1.5
+    try:
+        citations = response.response['citations']
+        sources = set([c['source'] for c in citations])
+    except KeyError:
+        # no citations provided and no placeholder available
+        # but not being evaluated by this criterion (see contains_number_citationS)
+        reward = -0.5
+        feedback = bad_message(f"Failed to provide the correct response formatting, see protocol definition.")
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
     # must cite correct citation urls
     score = 0.0
@@ -152,10 +190,32 @@ def contains_correct_number_of_citation_sources(validator: BaseValidatorNeuron, 
         feedback = good_message(f"You correctly identified some or all of the correct citation sources ({identified_sources}/{len(selected_sources)} identified).")
     else:
         feedback = bad_message(f"You failed to correctly identify any of the correct citation sources.")
-    return reward, feedback+received_reward_template.format(reward, max_reward)
+        feedback += f"\nYou should have found the following sources: {selected_sources}."
+        feedback += f"\nYou provided the following sources: {sources}."
+    return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
+
+# CRITERION: reward valid response - super simple check
+def correct_response_provided_simple(task, validator: BaseValidatorNeuron, response: bt.Synapse) -> [float, float, str]:
+    max_reward = 1.0
+    try:
+        response = response.response['response']
+    except KeyError:
+        # no citations provided and no placeholder available or maybe something wrong with the data sources
+        reward = -0.5
+        feedback = bad_message(f"You failed to provide the correct citation formatting - see protocal details.")
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
+
+    if task.response_should_contain in response:
+        reward = max_reward
+        feedback = good_message(f"You responded with a valid response from the provided context.")
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
+
+    reward = 0.0
+    feedback = bad_message(f"You failed to respond with a valid response from the provided context.")
+    return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
 # CRITERION: reward seemingly valid response (per a simple LLM (hosted by the validator))
-def correct_response_provided(validator: BaseValidatorNeuron, response: bt.Synapse, selected_datas: List[dict]) -> [float,str]:
+def correct_response_provided(task, validator: BaseValidatorNeuron, response: bt.Synapse, selected_datas: List[dict]) -> [float, float, str]:
     max_reward = 1.0
     try:
         citations = response.response['citations']
@@ -169,20 +229,20 @@ def correct_response_provided(validator: BaseValidatorNeuron, response: bt.Synap
         # no citations provided and no placeholder available or maybe something wrong with the data sources
         reward = -0.5
         feedback = bad_message(f"You failed to provide the correct citation formatting - see protocal details.")
-        return reward, feedback+received_reward_template.format(reward, max_reward)
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
     context = selected_datas[0]['context']
     input_text = f"Question: {prompt}\n\nAnswer: {completion}\n\nContext: {context}\n\n Is the answer to the question correct given the provided context, yes or no? Response:"
     yes_or_no = validator.validator_llm(input_text)
 
     if yes_or_no.strip().lower() == "yes":
-        reward = 1.0
+        reward = max_reward
         feedback = good_message(f"You responded with a valid response from the provided context.")
-        return reward, feedback+received_reward_template.format(reward, max_reward)
+        return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
     reward = 0.0
     feedback = bad_message(f"You failed to respond with a valid response from the provided context.")
-    return reward, feedback+received_reward_template.format(reward, max_reward)
+    return reward, max_reward, feedback+received_reward_template.format(reward, max_reward)
 
 # the core set of tests that form a set of criteria 
 def gen_data_task_criteria(selected_datas: List[dict], n_expected_citations:int) -> List[Criterion]:
@@ -202,6 +262,8 @@ default_criteria = [
 basic_citations = [
     Criterion(name="Must have at least one citation", desc="", eval_fx=contains_number_citations, eval_args=[1, None]),
     Criterion(name="Must have correct citation format", desc="", eval_fx=correct_citation_format),
+    Criterion(name="Must have correct citation source", desc="", eval_fx=contains_correct_citation_source),
 ]
 # not really used by can be
 basic_no_citations = Criterion(name="Must not return any citations", desc="", eval_fx=contains_number_citations, eval_args=[0, 0])
+simple_context_aware = Criterion(name="Must return a valid response baesd on context", desc="", eval_fx=correct_response_provided_simple)
