20848cef109f079cfadba66855b21d1141ad35af
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-01-09 06:34:14 +0000

    getting summary type data for summarization tasking

--- bitqna/validator/dataset.py
+++ bitqna/validator/dataset.py
@@ -21,34 +21,64 @@ import bittensor as bt
 from datasets import load_dataset
 from collections.abc import Iterator
 
-
-class Dataset(Iterator):
+class QnADataset(Iterator):
     def __init__(self):
         super().__init__()
         seed = random.randint(0, 1000)
-        self.openwebtext = iter(
-            load_dataset("openwebtext", split="train", streaming=True,trust_remote_code=True).shuffle(
-                seed=seed, buffer_size=10000
+        self.datasets = [ 
+            iter(
+                load_dataset("openwebtext", split="train", streaming=True,trust_remote_code=True).shuffle(
+                    seed=seed, buffer_size=10000
+                )
+            ),
+            iter(
+                load_dataset(
+                    "togethercomputer/RedPajama-Data-1T",
+                    "default",
+                    split="train",
+                    streaming=True,
+                    trust_remote_code=True,
+                ).shuffle(seed=seed, buffer_size=10000)
             )
-        )
-        self.red_pajama = iter(
-            load_dataset(
-                "togethercomputer/RedPajama-Data-1T",
-                "default",
-                split="train",
-                streaming=True,
-                trust_remote_code=True,
-            ).shuffle(seed=seed, buffer_size=10000)
-        )
+        ]
 
     def __next__(self):
+        bt.logging.debug("Retrieving data from dataset...")
         while True:
-            bt.logging.debug("Retrieving data from dataset...")
-            if random.random() < 0.5:
-                text = next(self.openwebtext)["text"]
-            else:
-                text = next(self.red_pajama)["text"]
+            ds = random.choice(self.datasets)
+            text = next(ds)["text"]
 
             # Check if the text is not empty or does not consist only of newline characters
             if text.strip():
                 return {"text": text}
+
+
+class SummaryDataset(Iterator):
+    def __init__(self):
+        super().__init__()
+        seed = random.randint(0, 1000)
+        self.keys = {"cnn_dailymail": {"text": "article", "summary": "highlights"},
+                     "samsum": {"text": "dialogue", "summary":"summary"}}
+        self.datasets = { 
+            "samsum": iter(
+                load_dataset("samsum", split="train", streaming=True,trust_remote_code=False).shuffle(
+                    seed=seed, buffer_size=10000
+                )
+            ),
+            "cnn_dailymail": iter(
+                load_dataset("cnn_dailymail", "3.0.0", split="train", streaming=True, trust_remote_code=False).shuffle(
+                    seed=seed, buffer_size=10000)
+            )
+        }
+
+    def __next__(self):
+        bt.logging.debug("Retrieving data from dataset...")
+        while True:
+            dname, ds = random.choice(list(self.datasets.items()))
+            data = next(ds)
+            text = data[self.keys[dname]["text"]]
+            summary = data[self.keys[dname]["summary"]]
+
+            # Check if the text is not empty or does not consist only of newline characters
+            if text.strip():
+                return {"text": text, "summary": summary}
