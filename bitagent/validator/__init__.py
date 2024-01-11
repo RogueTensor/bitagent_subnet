4181dc0245a397ff38088d135b0b9df1e3ddf914
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-01-11 03:56:29 +0000

    version numbers

--- bitagent/validator/__init__.py
+++ bitagent/validator/__init__.py
@@ -1,3 +1,10 @@
+__version__ = "0.1.0"
+version_split = __version__.split(".")
+__spec_version__ = (
+    (1000 * int(version_split[0]))
+    + (10 * int(version_split[1]))
+    + (1 * int(version_split[2]))
+)
 from .tasks import task
 from . import reward
 from .forward import forward
