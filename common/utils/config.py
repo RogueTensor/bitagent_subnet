1721fa2e44381faeaba9bfa028fb220a44b5bc67
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-01-11 05:12:10 +0000

    4 for now until we get more miners

--- common/utils/config.py
+++ common/utils/config.py
@@ -114,7 +114,7 @@ def add_args(cls, parser):
             "--neuron.sample_size",
             type=int,
             help="The number of miners to query in a single step.",
-            default=10,
+            default=4,
         )
 
         parser.add_argument(
