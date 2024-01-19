72af41fa20eed7ed8a7202b5e46443d83f51f00a
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-01-19 09:25:50 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -248,7 +248,7 @@ if [ "$?" -eq 1 ]; then
 
                         # Install latest changes just in case.
                         pip install -e .
-
+                        pip uninstall -y uvloop
                         # # Run the Python script with the arguments using pm2
                         # TODO (shib): Remove this pm2 del in the next spec version update.
                         pm2 del auto_run_validator
