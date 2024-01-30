ce5e258187ed80efe6cefe261a398ff887adf837
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-01-30 19:08:44 +0000

    leave option to be dynamic, but simplify for people just starting

--- neurons/miner.py
+++ neurons/miner.py
@@ -17,7 +17,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 import time
-import glob
+#import glob
 import argparse
 import importlib
 from typing import List, Tuple
@@ -51,8 +51,8 @@ class Miner(BaseMinerNeuron):
             "--miner",
             type=str,
             default="t5",
-            #choices=["t5", "mock"],
-            choices = [f.split("/")[-1].split("_")[0] for f in glob.glob("./bitagent/miners/*_miner.py")],
+            choices=["t5", "mock"],
+            #choices = [f.split("/")[-1].split("_")[0] for f in glob.glob("./bitagent/miners/*_miner.py")],
             help="Name/path of model to load."
         )
 
