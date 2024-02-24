4d3955637ff6e13790d48695db5085926adb6a74
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-02-24 11:23:28 -0700

    valis will do 25 miners at a time randomly

--- common/utils/config.py
+++ common/utils/config.py
@@ -114,7 +114,7 @@ def add_args(cls, parser):
             "--neuron.sample_size",
             type=int,
             help="The number of miners to query in a single step.",
-            default=11,
+            default=25, # was 11
         )
 
         parser.add_argument(
