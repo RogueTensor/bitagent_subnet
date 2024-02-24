f272c32f0ec6137ee2569e64dd7e9792b7df99c9
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-02-24 11:19:11 -0700

    better question asking for basic qna tasks

--- bitagent/validator/tasks/generated_qna_task.py
+++ bitagent/validator/tasks/generated_qna_task.py
@@ -35,10 +35,18 @@ class GeneratedQnATask(Task):
         datas = self.generate_random_texts(n_texts=n_texts)
         texts = [d['context'] for d in datas]
         sources = [d['source'] for d in datas]
-        selected_num=random.randrange(len(texts))
-        selected_text = texts[selected_num]
-        selected_source = sources[selected_num]
-        question = self.get_question_for_text(text=selected_text)
+        satisfied_with_question = False
+        loop_count = 0
+        while not satisfied_with_question:
+            loop_count += 1
+            selected_num=random.randrange(len(texts))
+            selected_text = texts[selected_num]
+            selected_source = sources[selected_num]
+            question = self.get_question_for_text(text=selected_text)
+            if loop_count > 10:
+                satisfied_with_question = True # just gonna go with it :(
+            else:
+                satisfied_with_question = self.check_question_for_alignment_with_text(question, text=selected_text)
     
         self.criteria=default_criteria+gen_data_task_criteria(selected_datas=[datas[selected_num]], n_expected_citations=n_expected_citations)
         self.synapse=QnATask(prompt=question, urls=[], datas=datas)
@@ -48,7 +56,7 @@ class GeneratedQnATask(Task):
         output = []
         for _ in range(n_texts):
             text = next(self.validator.qna_dataset)["text"]
-            text = text[:500]
+            text = text[:1500]
             # arbitrary and random source ids
             h = random.getrandbits(128)
             source = f'{time.strftime("%Y%m%d")}.bitagent.source.{h}'
@@ -58,22 +66,46 @@ class GeneratedQnATask(Task):
     def get_question_for_text(self, text: str) -> str:
         # for the simple model, use less text, truncate
         # current model (T5) takes 512 tokens, roughly 4 chars per token (per google PaLM 2), 1900 leaves us room for the rest of the prompt
-        truc_text = text[:1900]
+        truc_text = text[:3300]
         input_text = f"""
-            TEXT: {truc_text}\n\n
-            QUESTIONS TO NEVER ASK: DO NOT ask questions similar or remotely similar to the following:\n
-                - What is the topic of the article?\n
-                - What is the topic of the passage?\n
-                - What is the source of the information?\n
-                - What is the name of the author?\n
-                - What is the purpose of the video?\n
-                - What is the purpose of the article?\n
-                - What's the passage about?\n
-                - What's the game about?\n\n
-
-            TASK: Provide a 1-sentence question for the provided TEXT making sure to leverage key words from the TEXT in the question.  
-            Respond only with an insightful question leveraging keywords from the provided TEXT.\n
-            Response:
+            Ask a short 1-sentence question that is unique to this text, making sure to contain key phrases from the text such that the question can be answered by using this text and not other texts. Here is the text:
+            ``` 
+                {truc_text}
+            ```
+            Best Question:
         """
         question = self.validator.validator_llm(input_text)
         return question
+
+    def check_question_for_alignment_with_text(self, question:str, text:str):
+        score = self.validator.cross_encoder.predict([question, text])
+        if score < 0.3:
+            return False 
+
+        if "What is the main " in question:
+            return False
+
+        if "What are the main " in question:
+            return False
+
+        if "author's" in question:
+            return False
+
+        if len(question) < 20:
+            return False
+
+        input_text = f"""
+            Given this Question:
+            ```
+            {question}
+            ```
+            And this Context: 
+            ```
+            {text}
+            ```
+            Is the provided Question a strongly relevant question for the provided Context that is not a general question that can be ambiguous when selecting from a long list of text options.  In other words, are there enough details in the Question linked explicitly to the Context? Only respond with yes or no, no other words:
+        """
+        yes_or_no = self.validator.validator_llm(input_text)
+        if yes_or_no.strip().lower() == "yes":
+            return True
+        return False 
