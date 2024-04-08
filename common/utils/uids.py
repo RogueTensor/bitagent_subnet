380145b4987de82aba4c8883990cc92971921657
rizzo <154651009+RogueTensor@users.noreply.github.com>
2024-04-07 19:28:05 -0600

    any miner/uid receiving incentive will be tasked and evaluated

--- common/utils/uids.py
+++ common/utils/uids.py
@@ -18,13 +18,16 @@ def check_uid_availability(
     # Filter non serving axons.
     #if not metagraph.axons[uid].is_serving:
     #    return False
+    # don't hit sn owner
+    if uid == 0:
+        return False
+    # any miner receiving incentive should be queried
+    if metagraph.I[uid] > 0:
+        return True
     # Filter validator permit > 1024 stake.
     if metagraph.validator_permit[uid]:
         if metagraph.S[uid] > vpermit_tao_limit:
             return False
-    # don't hit sn owner
-    if uid == 0:
-        return False
     # Available otherwise.
     return True
 
