1580283bc809723ef178d6f47a086d067d18f5da
rizzo <154651009+RogueTensor@users.noreply.github.com>
2024-05-08 20:47:46 -0600

    new tasking

--- bitagent/task_api/tasks/__init__.py
+++ bitagent/task_api/tasks/__init__.py
@@ -3,4 +3,5 @@ from .basic_qna_tasks import *
 from .summary_task import *
 from .generated_qna_task import *
 from .generated_logic_qna_task import *
+from .generated_tool_selection_task import *
 #from .generated_agent_task import *
