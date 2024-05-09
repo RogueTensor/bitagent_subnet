d5e457511cc4b8259b5dbb3835a708f93f805489
rizzo <154651009+RogueTensor@users.noreply.github.com>
2024-05-08 20:48:58 -0600

    no more wandb, some local settings

--- bitagent/validator/initiation.py
+++ bitagent/validator/initiation.py
@@ -17,52 +17,15 @@
 
 import os
 import glob
-import wandb
 import shutil
 import bittensor as bt
-from datetime import datetime
+from bitagent.task_api.initiation import initiate_validator as initiate_validator_local
 
 # setup validator with wandb
 # clear out the old wandb dirs if possible
 def initiate_validator(self):
-    # clear out wandb runs that may be left over
-    def clear_wandb():
-        try:
-            bt.logging.debug("Clearing out stale wandb runs")
-            if os.path.exists("wandb"):
-                bt.logging.debug("Found the wandb dir...")
-                bt.logging.debug("Contents before delete: ", os.listdir("wandb"))
-                for f in glob.glob("wandb/run-*"):
-                    shutil.rmtree(f)
-                bt.logging.debug("Contents after delete: ", os.listdir("wandb"))
-            else:
-                bt.logging.debug('Could not find wandb dir, moving on')
-
-        except Exception as e:
-            bt.logging.debug(f"Error while trying to remove stale wandb runs: {e}")
-    clear_wandb()
-    self.clear_wandb = clear_wandb
-
-    # wandb setup
-    def init_wandb(miner_uid=None, validator_uid=None):
-        return None #wandb sucks
-        try:
-            run_name = f"{miner_uid}_{validator_uid}"
-            tags = ["validator_miner_runs", 
-                    f"validator_uid_{self.metagraph.hotkeys.index(self.wallet.hotkey.ss58_address)}",
-                    f"net_uid{self.config.netuid}",
-                    datetime.now().strftime('%Y_%m_%d'),
-            ]
-            if self.config.subtensor.network == "test" or self.config.netuid == 76: # testnet wandb
-                # bt.logging.debug("Initializing wandb for testnet")
-                return wandb.init(name=run_name, anonymous="allow", entity="bitagenttao", project="bitagent-testnet-logging", config=self.config, tags=tags)
-            elif self.config.subtensor.network == "finney" or self.config.netuid == 20: # mainnet wandb
-                # bt.logging.debug("Initializing wandb for mainnet")
-                return wandb.init(name=run_name, anonymous="allow", entity="bitagenttao", project="bitagent-logging", config=self.config, tags=tags)
-            else: # unknown network, not initializing wandb
-                # bt.logging.debug("Not initializing wandb, unknown network")
-                return None
-        except Exception as e:
-            #bt.logging.error("Wandb could not be initialized: ", e)
-            return None
-    self.init_wandb = init_wandb
+    if self.config.run_local:
+        def random_seed():
+            None
+        self.random_seed = random_seed
+        initiate_validator_local(self)
