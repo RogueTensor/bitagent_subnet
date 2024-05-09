03b4903845011812037fb8c61d0c4b276d8f57bd
rizzo <154651009+RogueTensor@users.noreply.github.com>
2024-05-08 20:50:31 -0600

    more task api checks

--- bitagent/validator/tasks/task.py
+++ bitagent/validator/tasks/task.py
@@ -21,44 +21,79 @@ import bittensor as bt
 from pprint import pformat
 from typing import List
 from bitagent.protocol import QnATask
+from bitagent.types import Tool
+
 
 # combines criterion/criteria plus QnATask synapse for eval to form a task for the miner
-class Task():
-    
+class Task:
+
     synapse: QnATask
 
-    def __init__(self, task_id: str, name: str, prompt: str, desc: str = "", task_type: str=None, 
-                datas: List[dict] = [], urls: List[str] = [], timeout: float = 12.0) -> None:
-        self.task_id=task_id
-        self.task_type=task_type
-        self.name=name
-        self.desc=desc
-        self.timeout=timeout
-        self.synapse=QnATask(prompt=prompt, urls=urls, datas=datas)
+    def __init__(
+        self,
+        task_id: str,
+        name: str,
+        prompt: str,
+        desc: str = "",
+        task_type: str = None,
+        datas: List[dict] = [],
+        tools: List[Tool] = [],
+        notes: str = "No Notes",
+        urls: List[str] = [],
+        timeout: float = 12.0,
+    ) -> None:
+        self.task_id = task_id
+        self.task_type = task_type
+        self.name = name
+        self.desc = desc
+        self.tools = tools
+        self.notes = notes
+        self.timeout = timeout
+        self.synapse = QnATask(
+            prompt=prompt, urls=urls, datas=datas, tools=tools, notes=notes
+        )
 
     @classmethod
     def create_from_json(cls, data):
-        return Task(data['task_id'], data['name'], data['prompt'], data['desc'], data['task_type'], data['datas'], data['urls'], data['timeout'])
+        return Task(
+            data["task_id"],
+            data["name"],
+            data["prompt"],
+            data["desc"],
+            data["task_type"],
+            data["datas"],
+            [Tool(**tool) for tool in data["tools"]],
+            data["notes"],
+            data["urls"],
+            data["timeout"],
+        )
 
     def reward(self, validator, response):
         num_retries = 0
         while num_retries < 3:
             try:
-                reward_url = f"{validator.config.task_api_host}/task_api/evaluate_task_response"
+                reward_url = (
+                    f"{validator.config.task_api_host}/task_api/evaluate_task_response"
+                )
                 headers = {"Content-Type": "application/json"}
                 data = {
                     "task_id": self.task_id,
-                    "response": {"response": response.response,
-                                "prompt": response.prompt,
-                                "urls": response.urls,
-                                "datas": response.datas,
-                                "dendrite_process_time": response.dendrite.process_time,
-                                "dendrite_status_code": response.dendrite.status_code,
-                                "axon_status_code": response.axon.status_code,
+                    "response": {
+                        "response": response.response,
+                        "prompt": response.prompt,
+                        "urls": response.urls,
+                        "datas": response.datas,
+                        "tools": response.tools,
+                        "notes": response.notes,
+                        "dendrite_process_time": response.dendrite.process_time,
+                        "dendrite_status_code": response.dendrite.status_code,
+                        "axon_status_code": response.axon.status_code,
                     },
                 }
 
-                result = requests.post(reward_url, headers=headers, json=data) #, verify=False)
+                result = requests.post(
+                    reward_url, headers=headers, json=data
+                )  # , verify=False)
                 result.raise_for_status()
                 return result.json()["result"]
             except KeyError as e:
@@ -71,40 +106,47 @@ class Task():
                 time.sleep(25)
                 num_retries += 1
         return False, "Waiting for Task API to come back online ..."
-        
 
     def __repr__(self):
         return pformat(vars(self), indent=4, width=1)
 
 def get_random_task(validator) -> [List[int], Task]:
-    while True:
-        try:
-            task_url = f"{validator.config.task_api_host}/task_api/get_new_task"
-            headers = {"Content-Type": "application/json"}
-            data = {}
+    if validator.config.run_local:
+        pass
+    else:
+        while True:
+            try:
+                task_url = f"{validator.config.task_api_host}/task_api/get_new_task"
+                headers = {"Content-Type": "application/json"}
+                data = {}
 
-            response = requests.post(task_url, headers=headers, json=data) #, verify=False)
-            if response.status_code == 502:
-                #bt.logging.debug("Task API is down, should be back up shortly: ", response)
-                time.sleep(25)
-            elif response.status_code != 200:
-                bt.logging.error("Error connecting to Task API, might be access restriction: ", response)
-                time.sleep(25)
-            else:
-                jdata = response.json()
-
-                # if miner_uids are empty, then leave empty and they will be randomly selected
-                if "miner_uids" in jdata.keys():
-                    # get miner uids to call for organic traffic
-                    miner_uids = jdata["miner_uids"]
-                    #bt.logging.debug("Received miner uids: ", miner_uids)
+                response = requests.post(
+                    task_url, headers=headers, json=data
+                )  # , verify=False)
+                if response.status_code == 502:
+                    # bt.logging.debug("Task API is down, should be back up shortly: ", response)
+                    time.sleep(25)
+                elif response.status_code != 200:
+                    bt.logging.error(
+                        "Error connecting to Task API, might be access restriction: ",
+                        response,
+                    )
+                    time.sleep(25)
                 else:
-                    miner_uids = []
+                    jdata = response.json()
 
-                task = Task.create_from_json(jdata["task"])
+                    # if miner_uids are empty, then leave empty and they will be randomly selected
+                    if "miner_uids" in jdata.keys():
+                        # get miner uids to call for organic traffic
+                        miner_uids = jdata["miner_uids"]
+                        # bt.logging.debug("Received miner uids: ", miner_uids)
+                    else:
+                        miner_uids = []
+                    task = Task.create_from_json(jdata["task"])
+                    return miner_uids, task
 
-                return miner_uids, task
-
-        except Exception as e:
-            bt.logging.debug("Likely waiting for Task API to come back online ... ", e)
-            time.sleep(25)
+            except Exception as e:
+                bt.logging.debug(
+                    "Likely waiting for Task API to come back online ... ", e
+                )
+                time.sleep(25)
