9dee40a695f2ad3f84aef85d0e76577eaa3ff99a
rizzo <154651009+RogueTensor@users.noreply.github.com>
2024-05-10 18:43:09 -0600

    schemas

---
+++ bitagent/schemas/tool.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Dict, Any
+
+class Tool(BaseModel):
+    """
+    Attributes:
+    - name: str
+    - description: str
+    - arguments: dict where the key is the name of the argument and the value is a dict containing the keys (required:bool, type:str, description:str)
+    """
+    name: str
+    description: str
+    arguments: Dict[str, Dict[str, Any]]
