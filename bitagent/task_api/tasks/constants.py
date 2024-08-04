0f2aeaa2cc1cc1a72beb45ec2c97b1eebf913c16
rizzo <154651009+RogueTensor@users.noreply.github.com>
2024-08-04 10:14:08 -0600

    halved task weights

--- bitagent/task_api/tasks/constants.py
+++ bitagent/task_api/tasks/constants.py
@@ -10,13 +10,13 @@ TASK_FREQUENCY = {
 
 TASK_WEIGHTS = {
     "basic_qna": 0.0,
-    "summary": 0.04,
-    "generated_qna": 0.09,
-    "generated_logic_qna": 0.03,
-    "tool_call": 0.1,
-    "tool_call_dataset": 0.025,
-    "tool_gen": 0.1,
-    "tool_gen_dataset": 0.025,
-    "conversation": 0.03,
-    "generated_plot_qna": 0.1,
+    "summary": 0.02,
+    "generated_qna": 0.045,
+    "generated_logic_qna": 0.015,
+    "tool_call": 0.05,
+    "tool_call_dataset": 0.015,
+    "tool_gen": 0.05,
+    "tool_gen_dataset": 0.015,
+    "conversation": 0.02,
+    "generated_plot_qna": 0.05,
 }
