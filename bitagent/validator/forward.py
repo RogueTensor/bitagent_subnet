839f139f973114a0c4f61813d2cac0328ec881fd
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-10-27 08:12:39 -0600

    wrap in try catch

--- bitagent/validator/forward.py
+++ bitagent/validator/forward.py
@@ -17,12 +17,11 @@
 # DEALINGS IN THE SOFTWARE.
 
 import asyncio
+import bittensor as bt
 
 from bitagent.validator.reward import process_rewards_update_scores_and_send_feedback
 from bitagent.tasks.task import get_random_task
 from common.utils.uids import get_random_uids
-
-from bitagent.tasks.task import get_random_task
 from bitagent.protocol import QueryTask
 
 async def forward(self, synapse: QueryTask=None) -> QueryTask:
@@ -35,26 +34,25 @@ async def forward(self, synapse: QueryTask=None) -> QueryTask:
 
     """
 
-    # TODO get all miner UIDs to compare against the TOP HF model
-    miner_uids = get_random_uids(self, k=min(self.config.neuron.sample_size, self.metagraph.n.item()))
-    task = get_random_task(self)
+    try:
 
-    #bt.logging.debug(f"Task prompt: {task.synapse.prompt[:2000]}")
+        # TODO get all miner UIDs to compare against the TOP HF model
+        miner_uids = get_random_uids(self, min(self.config.neuron.sample_size, self.metagraph.n.item()-2))
+        task = get_random_task(self)
 
-    # The dendrite client queries the network.
-    responses = self.dendrite.query(
-        # Send the query to selected miner axons in the network.
-        axons=[self.metagraph.axons[uid] for uid in miner_uids],
-        # Construct a query. 
-        synapse=task.synapse,
-        # All responses have the deserialize function called on them before returning.
-        # You are encouraged to define your own deserialization function.
-        deserialize=False,
-        timeout=task.timeout,
-    )
+        # The dendrite client queries the network.
+        responses = self.dendrite.query(
+            # Send the query to selected miner axons in the network.
+            axons=[self.metagraph.axons[uid] for uid in miner_uids],
+            # Construct a query. 
+            synapse=task.synapse,
+            # All responses have the deserialize function called on them before returning.
+            # You are encouraged to define your own deserialization function.
+            deserialize=False,
+            timeout=task.timeout,
+        )
 
-    # Generated Request
-    # When the task is generated, we need to evaluate the responses.
-    # Adjust the scores based on responses from miners.
-    # also gets results for feedback to the miners
-    await asyncio.create_task(process_rewards_update_scores_and_send_feedback(self, task=task, responses=responses, miner_uids=miner_uids))
+        await asyncio.create_task(process_rewards_update_scores_and_send_feedback(self, task=task, responses=responses, miner_uids=miner_uids))
+    except Exception as e:
+        bt.logging.error(f"Error in forward: {e}")
+        #raise e
