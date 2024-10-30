e4d921bf53d60e41aed37b0db0db40470f50369d
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2024-10-30 17:33:33 -0600

    setting model from validator and picking most frequently voted one

--- bitagent/miners/default_miner.py
+++ bitagent/miners/default_miner.py
@@ -17,49 +17,52 @@
 # DEALINGS IN THE SOFTWARE.
 
 import bitagent
-import bittensor as bt
+import numpy as np
+from bitagent.helpers.llms import llm
+from collections import Counter
 
 def miner_init(self, config=None):
-
-    def get_top_miner_HF_model_name():
-        if config.openai_model_name != "none":
-            return config.openai_model_name
-
-        top_miner_uid = self.metagraph.I.argmax()
-
-        top_miner_HF_model_name_response = self.dendrite.query(
-            # Send the query to selected miner axons in the network.
-            axons=[self.metagraph.axons[top_miner_uid]],
-            # Construct a query. 
-            synapse=bitagent.protocol.GetHFModelName,
-            # All responses have the deserialize function called on them before returning.
-            # You are encouraged to define your own deserialization function.
-            deserialize=False,
-            timeout=5.0,
-        )
-
-        return top_miner_HF_model_name_response[0].hf_model_name
-
-    # TODO handle tools along with the messages
-    def llm(messages, max_new_tokens = 160, temperature=0.7):
-        if isinstance(messages, str):
-            messages = [{"role":"user","content":messages}]
-
-        hf_model_name = get_top_miner_HF_model_name()
-        llm = ChatOpenAI(
-            openai_api_key=self.config.openai_api_key,
-            openai_api_base=self.config.openai_api_base,
-            model_name=hf_model_name,
-            max_tokens = max_new_tokens,
-            temperature = temperature,
-        )
-        return llm.invoke(messages).content.strip()
-
+    self.top_model_name = get_top_miner_HF_model_name(self)
     self.llm = llm
 
 def miner_process(self, synapse: bitagent.protocol.QueryTask) -> bitagent.protocol.QueryTask:
-
-    llm_response = self.llm(synapse.messages)
-    synapse.response["response"] = llm_response
+    llm_response = self.llm(self, synapse.messages, synapse.tools, self.top_model_name)
+    synapse.response = llm_response
 
     return synapse
+
+# each validator sends their top miner's HF model name to each miner
+def get_top_miner_HF_model_name(self):
+    # miner can specify a HF model name to run
+    if self.config.hf_model_name_to_run != "none":
+        return self.config.hf_model_name_to_run
+
+    # if no specific model name is specified, miner will use the top model name from the validators' votes
+    if not self.hf_top_model_names or len(self.hf_top_model_names) == 0:
+        return self.config.hf_model_name_to_run
+    else:
+        # get the most common model name
+        most_common_model_name = Counter(self.hf_top_model_names).most_common(1)[0][0]
+        return most_common_model_name
+
+def save_top_model_from_validator(self, top_hf_model_name, validator_uid):
+    # save off the top model from this validator
+    bt.logging.debug(f"Saving top HF model name from validator {validator_uid} state - {self.config.neuron.full_path}/miner_state.npz.")
+
+    self.hf_top_model_names[validator_uid] = top_hf_model_name
+
+    # Save the state of the miner to file.
+    np.savez(
+        self.config.neuron.full_path + "/miner_state.npz",
+        hf_top_model_names=self.hf_top_model_names,
+    )
+    
+def load_state(self):
+    """Loads the state of the miner from a file."""
+    bt.logging.info("Loading miner state.")
+    state = np.load(self.config.neuron.full_path + "/miner_state.npz")
+    if 'hf_top_model_names' in state:
+        loaded_hf_top_model_names = state["hf_top_model_names"]
+        self.hf_top_model_names = loaded_hf_top_model_names
+    else:
+        self.hf_top_model_names = {}
