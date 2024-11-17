50fd351f9b4fee9b1b1d980209abe4de15fcaf54
rizzo <154651009+RogueTensor@users.noreply.github.com>
2024-11-16 23:34:40 -0700

    comp constants

---
+++ bitagent/validator/constants.py
@@ -0,0 +1,4 @@
+COMPETITION_PREVIOUS_PREFIX = 1 # This is the previous competition prefix for when we swap to a new competition prefix and need to keep track of the old scores
+COMPETITION_PREFIX = 1
+DEPLOYED_DATE = "2024-11-16"
+COMPETITION_LENGTH_DAYS = 14
