22011e0e3ad1b7ca874cc5161456c4dfd1f56a2d
rizzo <154651009+RogueTensor@users.noreply.github.com>
2024-11-17 13:30:02 -0700

    setting to 2 weeks ago so we can have a previous version of 0s

--- bitagent/validator/constants.py
+++ bitagent/validator/constants.py
@@ -1,4 +1,4 @@
 COMPETITION_PREVIOUS_PREFIX = 1 # This is the previous competition prefix for when we swap to a new competition prefix and need to keep track of the old scores
 COMPETITION_PREFIX = 1
-DEPLOYED_DATE = "2024-11-16"
+DEPLOYED_DATE = "2024-11-02" # 2 weeks ago
 COMPETITION_LENGTH_DAYS = 14
