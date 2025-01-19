9072d652125d155b019fdcc233a081407b83b0f1
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2025-01-19 23:13:19 +0000

    Moved to 7 day competition cycle, start date adjusted to match competition index

--- bitagent/validator/constants.py
+++ bitagent/validator/constants.py
@@ -1,5 +1,5 @@
 COMPETITION_PREVIOUS_PREFIX = 1 # This is the previous competition prefix for when we swap to a new competition prefix and need to keep track of the old scores
 COMPETITION_PREFIX = 1
-DEPLOYED_DATE = "2024-11-05"
-COMPETITION_LENGTH_DAYS = 14
+DEPLOYED_DATE = "2024-12-10"
+COMPETITION_LENGTH_DAYS = 7
 TESTNET_COMPETITION_LENGTH_DAYS = 1.0/24.0 # every hour
