93a9e62d2c66b9b3cdba48e986ae1dabd98990be
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2025-02-24 22:45:42 +0000

    Up epoch length for weight setting to solve for block update latency

--- common/utils/config.py
+++ common/utils/config.py
@@ -116,7 +116,7 @@ def add_args(cls, parser):
         "--neuron.epoch_length",
         type=int,
         help="The default epoch length (how often we set weights, measured in 12 second blocks).",
-        default=100,
+        default=150,
     )
 
     parser.add_argument(
