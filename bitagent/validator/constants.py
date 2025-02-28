74243b73a5624e435778fdcfee1ac1b329ab5fbc
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2025-02-28 01:57:53 +0000

    Re-pull all models

--- bitagent/validator/constants.py
+++ bitagent/validator/constants.py
@@ -1,5 +1,5 @@
 COMPETITION_PREVIOUS_PREFIX = 1 # This is the previous competition prefix for when we swap to a new competition prefix and need to keep track of the old scores
-COMPETITION_PREFIX = 3
+COMPETITION_PREFIX = 4
 DEPLOYED_DATE = "2024-12-10"
 COMPETITION_LENGTH_DAYS = 10 ** 10
 TESTNET_COMPETITION_LENGTH_DAYS = 1.0/24.0 # every hour
