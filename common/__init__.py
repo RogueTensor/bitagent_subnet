963bab23ef2c9a0da2c00efce7fddf6f94e43d6b
RogueTensor <154651009+RogueTensor@users.noreply.github.com>
2025-04-15 15:33:36 +0000

    Spec version to 1.0.30

--- common/__init__.py
+++ common/__init__.py
@@ -17,7 +17,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 # Define the version of the common module.
-__version__ = "1.0.29"
+__version__ = "1.0.30"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
